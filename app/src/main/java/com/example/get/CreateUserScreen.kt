273e981d8d9d11c03c8aaa76c91100a77f6dbb16
Rupam <ddasrupam@gmail.com>
2025-03-12 18:42:52 +0530

    4rd commit

--- app/src/main/java/com/example/get/CreateUserScreen.kt
+++ app/src/main/java/com/example/get/CreateUserScreen.kt
@@ -39,7 +39,7 @@ fun CreateUserScreen(userViewModel: UserViewModel = viewModel()) {
             }
         })
         {
-            Text("submit")
+            Text("submit____")
         }
         response?.let {
             Text("Phone: ${it.phone}, Country ID: ${it.countryId}")
